4944418a235483e2d5187c8db3e6033f4f7b3e5a
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2020-10-08 23:49:02 +0500

    datatypes part 1
    basics

---
+++ src/DataTypes.kt
@@ -0,0 +1,21 @@
+
+   //Data types
+
+
+fun main(args : Array<String>){
+
+    //1. Numbers
+
+    val a :Int = 100
+    val b :Short = 2
+    val c :Long = 233330303
+    val d:Float
+    d = 23.45F
+    var e :Double = 3334.333
+
+    println("a is : "+a)
+    println("b is : "+b)
+    println("c is : "+c)
+    println("d is : "+d)
+    println("e is : "+e)
+}
